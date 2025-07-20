d9d6690bb580a57afd5111ad4581b12b95b03fe3
abubakar hamisu <hamisuabubakar27@gmail.com>
2025-07-20 20:22:35 +0000

    make all the option explain functional on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,9 +48,11 @@ export default function Home() {
           <span className="text-xl font-bold text-primary">Naija Pitch Connect</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
-          <Link href="/login" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
-            Login
-          </Link>
+          <Button asChild variant="ghost">
+            <Link href="/login" prefetch={false}>
+              Login
+            </Link>
+          </Button>
           <Button asChild>
             <Link href="/signup" prefetch={false}>
               Sign Up
