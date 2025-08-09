60e1202ff56ea30c85699e4579b0587fcd27207d
abubakar hamisu <hamisuabubakar27@gmail.com>
2025-08-09 10:27:25 +0000

    fix this

--- src/app/owner/dashboard/pitches/page.tsx
+++ src/app/owner/dashboard/pitches/page.tsx
@@ -41,6 +41,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import Image from "next/image"
 import Link from "next/link"
